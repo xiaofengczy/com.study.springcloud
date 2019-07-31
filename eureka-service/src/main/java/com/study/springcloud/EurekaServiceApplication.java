ce6a85b9a6d0468e5ce311c7b968d7294c9ae035
曹中余 <caozhongyu@yunsom.com>
2019-07-31 08:19:13 +0800

    Initial commit

---
+++ eureka-service/src/main/java/com/study/springcloud/EurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.study.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * FileName: EurekaServiceApplication Description:
+ *
+ * @author caozhongyu
+ * @create 2019-07-31
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+
+}
